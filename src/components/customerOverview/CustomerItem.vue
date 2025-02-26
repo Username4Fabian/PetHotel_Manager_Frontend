bdef1925c85e72b0401a23e1f28664148ebd432c
Username4Fabian <03anion.proxys@icloud.com>
2025-02-26 21:35:56 +0100

    fixed design

--- src/components/customerOverview/CustomerItem.vue
+++ src/components/customerOverview/CustomerItem.vue
@@ -22,7 +22,8 @@ const toggleDetails = () => {
 };
 
 const deleteCustomer = async () => {
-  if (window.confirm(`Sind Sie sicher, dass Sie den Kunden: ${props.customer.firstName} ${props.customer.lastName} löschen möchten?`)) {
+  const confirmationMessage = `Sind Sie sicher, dass Sie den Kunden: ${props.customer.firstName} ${props.customer.lastName} löschen möchten?\n\nAlle dem Kunden angehörige Hunde werden ebenfalls gelöscht.\n\nDiese Aktion kann nicht rückgängig gemacht werden.`;
+  if (window.confirm(confirmationMessage)) {
     try {
       await axios.delete(`/api/kunde/DeleteKunde/${props.customer.id}`);
       emits('customerDeleted', props.customer);
@@ -49,15 +50,15 @@ const handleUpdateCustomer = (updatedCustomer) => {
       </div>
       <div class="flex space-x-2">
         <button
-          v-if="actionType === 'delete'"
-          @click.stop="deleteCustomer"
-          class="text-gray-500 hover:text-red-600 transition-colors duration-200 text-3xl hover:cursor-pointer">
-          <i class="fas fa-trash-alt"></i>
+          @click.stop="showEditOverlay = true"
+          class="text-gray-500 hover:text-blue-600 transition-colors duration-200 text-3xl hover:cursor-pointer mr-10">
+          <i class="fas fa-edit"></i>
         </button>
         <button
-          @click.stop="showEditOverlay = true"
-          class="text-gray-500 hover:text-blue-600 transition-colors duration-200 text-3xl hover:cursor-pointer">
-          <i class="fas fa-edit"></i>
+          v-if="actionType === 'delete'"
+          @click.stop="deleteCustomer"
+          class="text-gray-500 hover:text-red-600 transition-colors duration-200 text-3xl hover:cursor-pointer mr-5">
+          <i class="fas fa-trash-alt"></i>
         </button>
       </div>
     </div>
